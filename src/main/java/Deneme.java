e5e4a368b3aa1d75525db8afa0d02eecfd1c2b9c
OmerFarukEkici58 <ekiciomerfaruk.45@gmail.com>
2023-09-05 12:11:10 +0300

    05.09.2023

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -1,20 +1,2 @@
 public class Deneme {
-    static int num1;
-    String name = "Ali";
-    public static void main(String[] args){
-        add();
-//        product(5);
-        System.out.println("num1 = " + num1);
-    }
-    public static void add(){
-        num1++;
-        int num2 = 6;
-        char letter;
-        System.out.println("Do addition");
-    }
-//    public void product(int num3){
-//        name = "Veli";
-//        num2++;
-//        System.out.println(num3 * num3);
-//    }
 }
